c18004d685fb071a478dcdbee126fdd9d5292757
cristian conde <condecristian126@gmail.com>
2026-05-23 11:14:37 -0500

    ajustamos conexion firebase en base64

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -7,7 +7,11 @@ import admin from 'firebase-admin';
 const initFirebase = () => {
   if (admin.apps.length > 0) return admin.app();
 
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  // const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf8')
+);
 
   return admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
